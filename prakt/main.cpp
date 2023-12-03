242e2418d5450360cde5599aa78baa6b812a4afe
VadymPavlyshyn <vadumpavlishin@gmail.com>
2023-12-03 21:02:58 +0200

    main.cpp

--- prakt/main.cpp
+++ prakt/main.cpp
@@ -4,42 +4,21 @@
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "uk_UA.utf8");
-	int id;
-	string title;
-	string author;
-	string publisher;
-	int yearOfPublication;
-	int numberOfPages;
-	int prise;
-	bool availabilityOfHardcover;
 
 
 
 
-	cout << "Запис книги" << endl;
-	cout << "id" << endl;
-	cin >> id;
-	cout << "Назва" << endl;
-	cin >> title;
-	cout << "Автор" << endl;
-	cin >> author;
-	cout << "Видавництво" << endl;
-	cin >> publisher;
-	cout << "Рік публікації" << endl;
-	cin >> yearOfPublication;
-	cout << "Кількість сторінок" << endl;
-	cin >> numberOfPages;
-	cout << "Ціна" << endl;
-	cin >> prise;
-	cout << "Наявність тверої обкладинки  true/false" << endl;
-	cin >> availabilityOfHardcover;
+	
+	Book book;
+	
+	Audiobook audiobook;
+	audiobook.input();
+	book.input();
+	
 
-
-
-	Book book(id, title, author, publisher, yearOfPublication, numberOfPages, prise, availabilityOfHardcover);
-
+	
 	book.print();
-
+	audiobook.print();
 
 
 
